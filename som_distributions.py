c1da4777a90306579c9a917dc115c0ca5a4fc0bb
marco7877 <56403434+marco7877@users.noreply.github.com>
2020-03-31 00:39:48 -0600

    Update som_distributions.py

--- som_distributions.py
+++ som_distributions.py
@@ -22,11 +22,11 @@ def charge_som(som_text):
 
 def dist(v1, v2):
     return math.sqrt(sum((v1 - v2) ** 2))
-def closest_node(stimuli, map, m_rows, m_cols):
+def closest_node(stimuli, map, m_rows):
     result=(0,0)
     small_dist = dist(stimuli,map[0][0])#número inicial de distancia pequeña
-    for i in range(int(m_rows)):
-      for j in range(int(m_cols)):
+    for i in list(range(0,int(m_rows))):
+      for j in list(range(0,int(m_rows))):
         ed = dist(map[i][j], stimuli)
         if ed < small_dist:# compra distancia euclideana para saber su fitness
           small_dist = ed# si sí, la menor distancia se reescribe
@@ -50,11 +50,7 @@ def plothist(axis_dictionary,labelslist,axis):
 def dict2list(dictionary,key):
     mylist=list(dictionary[key].values())
     return mylist
-normalize=False
-data_file = "/media/marco/MarcoHDD/github/SOM/SOMoutput/SOM_1000_noise2_gaussianRaw_.txt"
-data_som = np.loadtxt(data_file, delimiter=",",
-                      dtype=np.floating)
-map,dim,lado=charge_som(data_som)
+
 def plotdist(list_ocurr,plotname):    
     mu = np.mean(list_ocurr)
     sigma = np.mean(list_ocurr)
@@ -78,9 +74,6 @@ def plotdist(list_ocurr,plotname):
     ax.set_title('Normal Gaussian Curve')
     #plt.savefig('normal_curve.png', dpi=72, bbox_inches='tight')
     plt.show()
-data_stimulifile = "/media/marco/MarcoHDD/github/LipReading/output/ArtificialDescriptorBa2_gaussian.csv"
-data_stimuli = np.loadtxt(data_stimulifile, delimiter=",", usecols=range(0,dim),
-                    dtype=np.floating)# vector por sujeto
 
 def z_comparition (lt):
     ocurr_array=np.array(lt)
@@ -89,9 +82,6 @@ def z_comparition (lt):
     sd=np.std(ocurr_array)
     z=(median-mean)/sd
     return z
-if normalize==True:
-    maxim=np.amax(data_stimuli)
-    data_stimuli=normal(data_stimuli,maxim)
 def scatter_hist(x, y, ax, ax_histx, ax_histy):
     ax_histx.tick_params(axis="x", labelbottom=False)
     ax_histy.tick_params(axis="y", labelleft=False)
@@ -115,32 +105,96 @@ def scatter_z(x, y,syl):
     ax_histx = fig.add_axes(rect_histx, sharex=ax)
     ax_histy = fig.add_axes(rect_histy, sharey=ax)
     scatter_hist(x, y, ax, ax_histx, ax_histy)
-    plt.title("scatter z "+syl)
+    plt.title("scatter z "+str(syl))
     plt.show()
+def ocurrences(data_stimuli,map,lado,labels_size,labels):
+    result_x=axis_dict([1,2],lado)
+    result_y=axis_dict([1,2],lado)
+    ocurr_listx1=[]
+    ocurr_listX2=[]
+    ocurr_listy1=[]
+    ocurr_listy2=[]
+    for i in list(range(0,labels_size)):
+        winner=closest_node(data_stimuli[i], map, lado)
+        key=labels[i]
+        x,y=winner
+        if key==1:
+           ocurr_listx1.append(x)
+           ocurr_listy1.append(y)
+        else:
+           ocurr_listX2.append(x)
+           ocurr_listy2.append(y)
+        result_x[key][x]+=1
+        result_y[key][y]+=1
+    return result_x, result_y,ocurr_listx1, ocurr_listy1, ocurr_listX2, ocurr_listy2
+
+def divergencekl(listocurr_p,listocurr_q):
+    ocurr_p=listocurr_p
+    ocurr_q=listocurr_q
+    n_p=len(ocurr_p)
+    n_q=len(ocurr_q)
+    for i in list(range(0,n_p)):
+        if ocurr_p[i] !=0: 
+            ocurr_p[i]=ocurr_p[i]/n_p
+        else:
+            ocurr_p[i]=0
+    for i in list(range(0,n_p)):
+        if ocurr_q[i] !=0: 
+            ocurr_q[i]=ocurr_q[i]/n_q
+        else:
+            ocurr_q[i]=0
+    for i in list(range(0,n_p)):
+        if ocurr_p[i]==0:
+            ocurr_p[i]=1
+    for i in list(range(0,n_q)):
+        if ocurr_q[i]==0:
+            ocurr_q[i]=1
+    ocurr_p=np.log2(ocurr_p)
+    ocurr_q=np.log2(ocurr_q)
+    dkl=0
+    for i in list(range(0,n_p)):
+        dkl+=ocurr_p[i]-ocurr_q[i]
+    return dkl
+def sctter_hist(x, y, ax, ax_histx, ax_histy):
+    ax_histx.tick_params(axis="x", labelbottom=True)
+    ax_histy.tick_params(axis="y", labelleft=True)
+    ax.scatter(x, y)
+    ax_histx.hist(x, bins=10)
+    ax_histy.hist(y, bins=10, orientation='horizontal')
+    return ax_histx, ax_histy,ax
+
+###############################main############################################
+data_file = "/media/marco/MarcoHDD/github/SOM/SOMoutput/SOM_1000_noise2_gaussianRaw_.txt"
+data_som = np.loadtxt(data_file, delimiter=",",
+                      dtype=np.floating)
+map,dim,lado=charge_som(data_som)
+data_stimulifile = "/media/marco/MarcoHDD/github/LipReading/output/ArtificialDescriptorBa2_gaussian.csv"
+data_stimuli = np.loadtxt(data_stimulifile, delimiter=",", usecols=range(0,dim),
+                    dtype=np.floating)# vector por sujeto
+data_stimulifileo = "/media/marco/MarcoHDD/github/LipReading/output/Descriptor_baga_original.csv"
+data_stimulio = np.loadtxt(data_stimulifileo, delimiter=",", usecols=range(0,dim),
+                    dtype=np.floating)# vector por sujeto
+data_stimulifilea = "/media/marco/MarcoHDD/github/LipReading/output/Descriptor_baga_artificial.csv"
+data_stimulia = np.loadtxt(data_stimulifilea, delimiter=",", usecols=range(0,dim),
+                    dtype=np.floating)# vector por sujeto
+normalize=False
+if normalize==True:
+    maxim=np.amax(data_stimuli)
+    data_stimuli=normal(data_stimuli,maxim)
     
+
+
 labels = np.loadtxt(data_stimulifile, delimiter=",", usecols=(dim),dtype=np.int)
 _x_=labels.size
-result_x=axis_dict([1,2],lado)
-result_y=axis_dict([1,2],lado)
-ocurr_x={1:[],2:[]}
-ocurr_y={1:[],2:[]}
-ocurr_listx1=[]
-ocurr_listX2=[]
-ocurr_listy1=[]
-ocurr_listy2=[]
-for i in list(range(0,_x_)):
-    winner=closest_node(data_stimuli[i], map, lado, lado)
-    key=labels[i]
-    x,y=winner
-    if key==1:
-        ocurr_listx1.append(x)
-        ocurr_listy1.append(y)
-    else:
-        ocurr_listX2.append(x)
-        ocurr_listy2.append(y)
-    result_x[key][x]+=1
-    result_y[key][y]+=1
-    
+labelso = np.loadtxt(data_stimulifileo, delimiter=",", usecols=(dim),dtype=np.int)
+_xo_=labelso.size
+labelsa = np.loadtxt(data_stimulifilea, delimiter=",", usecols=(dim),dtype=np.int)
+_xa_=labelsa.size
+result_x, result_y,ocurr_listx1, ocurr_listy1, ocurr_listX2, ocurr_listy2= ocurrences(data_stimuli,map,lado,_x_,labels)
+result_xo, result_yo,ocurr_listx1o, ocurr_listy1o, ocurr_listX2o, ocurr_listy2o= ocurrences(data_stimulio,map,10,_xo_,labelso)
+result_xa, result_ya,ocurr_listx1a, ocurr_listy1a, ocurr_listX2a, ocurr_listy2a= ocurrences(data_stimulia,map,10,_xa_,labelsa)
+
+
 
 plothist(result_x,[1,2],"x")
 plothist(result_y,[1,2],"y")    
@@ -148,6 +202,14 @@ x_ba=dict2list(result_x,1)
 x_ga=dict2list(result_x,2)
 y_ba=dict2list(result_y,1)
 y_ga=dict2list(result_y,2)
+x_bao=dict2list(result_xo,1)
+x_gao=dict2list(result_xo,2)
+y_bao=dict2list(result_yo,1)
+y_gao=dict2list(result_yo,2)
+x_baa=dict2list(result_xa,1)
+x_gaa=dict2list(result_xa,2)
+y_baa=dict2list(result_ya,1)
+y_gaa=dict2list(result_ya,2)
 plotdist(ocurr_listx1,"x_ba")
 plotdist(ocurr_listy1,"y_ba")
 plotdist(ocurr_listX2,"x_ga")
@@ -160,41 +222,29 @@ zY2=z_comparition(ocurr_listy2)
 scatter_z(zx1,zy1,'ba')
 scatter_z(zx2,zy1,'ga')
 
-
-'''
-hist, bin_edges=np.histogram(ocurr_x,bins=10)
-import matplotlib.pyplot as plt
-plt.bar(bin_edges[:-1], hist, width = 1)
-plt.xlim(min(bin_edges), max(bin_edges))
-plt.show()
-'''
-def sctter_hist(x, y, ax, ax_histx, ax_histy):
-    ax_histx.tick_params(axis="x", labelbottom=False)
-    ax_histy.tick_params(axis="y", labelleft=False)
-    ax.scatter(x, y)
-    binwidth = 0.4
-    xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
-    lim = (int(xymax/binwidth) + 1) * binwidth
-    bins = np.arange(-lim, lim + binwidth, binwidth)
-    ax_histx.hist(x, bins=bins)
-    ax_histy.hist(y, bins=bins, orientation='horizontal')
-left, width = 0.1, 0.65
-bottom, height = 0.1, 0.65
-spacing = 0.06
-
-
-rect_scatter = [left, bottom, width, height]
-rect_histx = [left, bottom + height + spacing, width, 0.3]
-rect_histy = [left + width + spacing, bottom, 0.3, height]
-
+def plot_sctter(ocurr_listX2, ocurr_listy2,name):
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    spacing = 0.06
+    rect_scatter = [left, bottom, width, height]
+    rect_histx = [left, bottom + height + spacing, width, 0.3]
+    rect_histy = [left + width + spacing, bottom, 0.3, height]
 # start with a square Figure
-fig = plt.figure(figsize=(10,10))
-
-ax = fig.add_axes(rect_scatter)
-ax_histx = fig.add_axes(rect_histx)
-ax_histy = fig.add_axes(rect_histy)
-
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_axes(rect_scatter)
+    ax_histx = fig.add_axes(rect_histx)
+    ax_histy = fig.add_axes(rect_histy)
 # use the previously defined function
-sctter_hist(ocurr_listx1, ocurr_listy1, ax, ax_histx, ax_histy)
-plt.title("som ba activation")
-plt.show()
+    ax_histx, ax_histy,ax=sctter_hist(ocurr_listX2o, ocurr_listy2o, ax, ax_histx, ax_histy)
+    plt.title("scatter plot for "+str(name))
+    plt.show()
+
+klbax=divergencekl(x_bao,x_ba)
+klbay=divergencekl(y_bao,y_ba)
+klgax=divergencekl(x_gao,x_ga)
+klgay=divergencekl(y_gao,y_ga)
+print(klbax,klbay,klgax,klgay)
+
+plot_sctter(ocurr_listX2o, ocurr_listy2o, name="empirical ga stimuli")
+plot_sctter(ocurr_listx1o, ocurr_listy1o, name="empirical ba stimuli")
+name="empirical ga stimuli"
